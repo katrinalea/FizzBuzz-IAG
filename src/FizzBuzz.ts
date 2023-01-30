4f2a9ed81fcb8d1d4a87ba873bd36404f3cf344c
Katrina <katrinalea.woolley@yahoo.co.uk>
2023-01-30 10:32:45 +0000

    formatting

--- src/FizzBuzz.ts
+++ src/FizzBuzz.ts
@@ -4,16 +4,15 @@ export const fizzBuzz = (n: number): string => {
   const newArray: IfizzBuzz[] = [];
 
   for (let num = 1; num <= n; num++) {
-    if (num % 3 === 0 && num % 5 === 0) {
-      newArray.push("FizzBuzz");
-    } else if (num % 3 === 0) {
-      newArray.push("Fizz");
-    } else if (num % 5 === 0) {
-      newArray.push("Buzz");
-    } else {
-      newArray.push(num);
-    }
+    num % 3 === 0 && num % 5 === 0
+      ? newArray.push("FizzBuzz")
+      : num % 3 === 0
+      ? newArray.push("Fizz")
+      : num % 5 === 0
+      ? newArray.push("Buzz")
+      : newArray.push(num);
   }
+
   return newArray.toString();
 };
 
